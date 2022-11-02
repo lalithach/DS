d211353342f184620d6e500a96bc53dfed35364b
lalithach <lalitha.chekka7@gmail.com>
2022-11-02 14:32:51 +0530

    Update dutch_national_flag.py

--- dutch_national_flag.py
+++ dutch_national_flag.py
@@ -1,10 +1,12 @@
+import random
 # 3way lomuto partitioning
 def lomuto(A, start, end):
 	orange=start
 	blue = start
 	pivot = A[start]
-	for green in range(start+1, end+1) :
+	for green in range(start+1, end+1) :  
 		if A[green] == pivot:
+			# When initial element after pivot is lesser, then orange zone increases. Since blue region falls after orange
 			if blue==0:
 				blue = orange + 1
 			else:
@@ -13,24 +15,27 @@ def lomuto(A, start, end):
 		elif A[green] > pivot:
 			orange+=1
 			A[green],A[orange]=A[orange],A[green]
+			# There is a chance that the swapped element above could be pivot which could disturb the order
 			if A[green] == pivot:
 				blue +=1
 				A[blue], A[green] = A[green], A[blue]
 		else:
 			pass
-		#print(f"Index: {green}: {A}")
 	return orange, blue
 
-def helper(A, start, end):
-  #leaf worker
+def quickselect(A, start, end):
+  	#leaf worker
 	if start>=end:
 		return
 	#arbitary worker
+	# swap pivot with random index no, to avoid worst case
+	randind = random.randint(start,end)
+	A[start], A[randind] = A[randind], A[start]
 	orange, blue = lomuto(A, start, end)
 	A[orange], A[start] = A[start], A[orange]
-	#print(A)
-	helper(A, start, orange-1)
-	helper(A, blue+1, end)
+
+	quickselect(A, start, orange-1)
+	quickselect(A, blue+1, end)
 
 def dutch_flag_sort(balls):
     """
@@ -40,7 +45,7 @@ def dutch_flag_sort(balls):
      list_char
     """
     # Write your code here.
-    helper(balls,0,len(balls)-1)
+    quickselect(balls,0,len(balls)-1)
     return balls
 
  
