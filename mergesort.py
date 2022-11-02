f887ac55978b273fcaff18f005c2821a92e49df8
lalithach <lalitha.chekka7@gmail.com>
2022-11-02 22:49:15 +0530

    Update mergesort.py

--- mergesort.py
+++ mergesort.py
@@ -1,36 +1,47 @@
-def merge_sort(array):
-    if len(array) < 2:
-        return array
 
-    # get the mid of the array
-    midpoint = len(array) // 2
-    
-    # recursive call until the array is broken into a list of len 1
-    return merge(merge_sort(array[:midpoint]), merge_sort(array[midpoint:]))
-
-def merge(left,right):
-    l = len(left)
-    m = len(right)
-    result = []
-    left_index=right_index=0
-    while(len(result)<=(l+m)):
-        # first element of both lists is compared 
-        # append the lesser one and increment the index
-        if left[left_index]<=right[right_index]:
-            result.append(left[left_index])
-            left_index += 1
+def helper(arr, start, end):
+    # leaf worker
+    if (start == end):
+        return
+    # arbitary worker
+    mid = (start+end)//2
+    # delegate
+    helper(arr, start, mid)
+    helper(arr, mid+1, end)
+    # combine
+    i = start
+    j = mid+1
+    aux_arr = []
+    # compare first half with second half
+    # The lesser element goes to aux array
+    while i<=mid and j<=end:
+        if arr[i] <= arr[j]:
+            aux_arr.append(arr[i])
+            i+=1
         else:
-            result.append(right[right_index])
-            right_index += 1
-        # copy the other list contents into the result
-        # if a list reaches its end
-        if left_index == l:
-            result += right[right_index:]
-            break
-        if right_index == m:
-            result += left[left_index:]
-            break
-    return result
+            aux_arr.append(arr[j])
+            j+=1
+            
+    # if in one half, index reaches the end,
+    # append contents of other half
+    while i<=mid:
+        aux_arr.append(arr[i])
+        i+=1
+    while j<=end:
+        aux_arr.append(arr[j])
+        j+=1
 
+    arr[start:end+1] = aux_arr
+    return
+    
+def merge_sort(arr):
+    """
+    Args:
+     arr(list_int32)
+    Returns:
+     list_int32
+    """
+    # Write your code here.
+    helper(arr, 0, len(arr)-1)
+    return arr
 
-print(merge_sort([9,8,7,6,5,4,3,2,1]))  # prints [1,2,3,4,5,6,7,8,9]
